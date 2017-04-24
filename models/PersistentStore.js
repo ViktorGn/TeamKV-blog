3d49a4456866d5a275ed5d8b23ed61a97a3015f2
ViktorGn <vgtest@yahoo.com>
2017-04-24 06:35:07 +0300

    Persistent storage added - for future use.

---
+++ models/PersistentStore.js
@@ -0,0 +1,20 @@
+/**
+ * Created by VG on 24-Apr-17.
+ */
+//Keep application parameters and configuration
+const mongoose = require('mongoose');
+
+let PersistentStoreSchema = mongoose.Schema({
+    articleAutoCount: {type: Number, default:1},
+    categories:{type:[String]},
+    conf_id:{type: Number, default:1}
+});
+
+var PersistentStore = mongoose.model('PersistentStore', PersistentStoreSchema);
+//var seedRec = new PersistentStore({
+//articleAutoCount: 1,
+//categories: ["General"]
+//});
+//seedRec.save();
+//console.log("Persistent")
+module.exports = PersistentStore;
